ec3a03bb932d80d8e3a511caf73509039cfb46da
James Conant <jfaconant@gmail.com>
2016-09-18 17:08:20 -0400

    Fix UID/GID bug

--- attributes/user.rb
+++ attributes/user.rb
@@ -1,7 +1,9 @@
+# id 1000 taken by chef user
+
 default['desktop']['user'].tap do |user|
   user['name'] = 'jconant'
-  user['uid'] = '1000'
+  user['uid'] = '1001'
   user['group'] = 'jconant'
-  user['gid'] = '1000'
+  user['gid'] = '1001'
   user['home'] = '/home/jconant'
 end
