03fefb0ae75c20afa7fd34e4ce9f4218f03c80e9
James Conant <jfaconant@gmail.com>
2016-09-23 19:24:33 -0400

    Revert configs over-write commit

--- recipes/configs.rb
+++ recipes/configs.rb
@@ -10,17 +10,6 @@ include_recipe 'chef-workstation::user'
 desktop_user = node['desktop']['user']['name']
 desktop_group = node['desktop']['user']['group']
 
-# delete existing configs
-["bashrc", "vimrc", "gitconfig", "git_bash_completion"].each do |config|
-  template "/home/#{desktop_user}/.#{config}" do
-    owner desktop_user
-    group desktop_group
-    source "home/#{config}"
-    action :delete
-  end
-end
-
-# install current configs
 ["bashrc", "vimrc", "gitconfig", "git_bash_completion"].each do |config|
   template "/home/#{desktop_user}/.#{config}" do
     owner desktop_user
