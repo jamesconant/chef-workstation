f9fe40e20b03a2ab0fb7ff595c6785c2d0d5be05
James Conant <jfaconant@gmail.com>
2016-09-29 19:56:39 -0400

    remove custom font

--- recipes/i3.rb
+++ recipes/i3.rb
@@ -9,23 +9,6 @@
 
 # include_recipe 'chef-workstation::apt'
 #
-# package 'xorg'
-# package 'xdm'
-# package 'i3'
-
-package 'unzip'
-package 'rsync'
-
-# remote_file "/tmp/yosemite_fonts.zip" do
-#   source 'https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip'
-#   # user node['desktop']['user']['name']
-#   # group node['desktop']['user']['group']
-#   not_if{ File.exists?("tmp/yosemite_fonts.zip") }
-# end
-
-ark 'YosemiteSanFranciscoFont-master' do
-  url 'https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip'
-  path '/usr/local/share/fonts'
-  creates 'System San Francisco Display Thin.ttf'
-  action :cherry_pick
-end
+package 'xorg'
+package 'xdm'
+package 'i3'
