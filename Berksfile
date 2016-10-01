c8f1b94a080ac9267873b52884b14000bcd22374
James Conant <jfaconant@gmail.com>
2016-10-01 09:20:00 -0400

    more WIP

--- Berksfile
+++ Berksfile
@@ -1,5 +1,13 @@
 source 'https://supermarket.chef.io'
 
-cookbook 'desktop', git: 'git@github.com:http-418/chef-desktop'
+#
+# This wrapper cookbook is intended for itself to be wrapped as well, and not ran directly.
+# Because Berkshelf does not handle nested Berksfiles (1), the desktop dependency needs to be
+# specified in the top-level wrapper cookbook.
+#
+# (1) https://github.com/berkshelf/berkshelf/issues/1174
+#
+# cookbook 'desktop', git: 'git@github.com:http-418/chef-desktop'
+#
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,15 @@ description      'Installs/Configures workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.5.6'
 
+#
+# This wrapper cookbook is intended for itself to be wrapped as well, and not ran directly.
+# Because Berkshelf does not handle nested Berksfiles (1), the desktop dependency needs to be
+# specified in the top-level wrapper cookbook.
+#
+# (1) https://github.com/berkshelf/berkshelf/issues/1174
+#
 # depends 'desktop'
+#
 depends 'debian', '~> 1.8'
 depends 'ruby_build'
 depends 'ruby_rbenv'
