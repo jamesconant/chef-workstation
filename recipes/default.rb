167b85c2073f724b62053c82c6f83cfc7ac839cb
James Conant <jfaconant@gmail.com>
2016-10-01 16:27:19 -0400

    Replace workstation::ruby with desktop::ruby

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,6 +22,6 @@ include_recipe 'desktop::apt'
 include_recipe 'desktop::user'
 include_recipe 'workstation::configs'
 include_recipe 'workstation::i3'
-include_recipe 'workstation::ruby'
+include_recipe 'desktop::ruby'
 
 package 'vim'
