c44ae575e2533394c77dfb682aabceca2f73c81b
James Conant <jfaconant@gmail.com>
2016-10-01 09:00:02 -0400

    wip

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.5.6'
 
-depends 'desktop'
+# depends 'desktop'
 depends 'debian', '~> 1.8'
 depends 'ruby_build'
 depends 'ruby_rbenv'
