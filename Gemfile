e38f669ae3cd9e57439b9bc32e508fd0c51ff780
James Conant <jfaconant@gmail.com>
2016-09-30 22:20:03 -0400

    Clean up tests - rely on VM rather than docker

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'berkshelf', '~> 4'
 gem 'test-kitchen', '~> 1'
 gem 'kitchen-inspec'
+gem 'kitchen-docker'
 
 group :integration do
   gem 'kitchen-vagrant', '~> 0.19'
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jfaconant@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.2'
+version          '0.5.3'
 
 depends 'debian', '~> 1.8'
 depends 'ruby_build'
