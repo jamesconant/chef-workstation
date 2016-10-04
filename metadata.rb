c34369d0596b20829568dbad032ee449f9931d9a
James Conant <jfaconant@gmail.com>
2016-10-03 20:58:41 -0400

    Update bashrc

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jfaconant@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.12'
+version          '0.5.13'
 
 #
 # This wrapper cookbook is intended for itself to be wrapped as well, and not ran directly.
