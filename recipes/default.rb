ecf0d243902260c0175c1ca73abc32231431d849
James Conant <jfaconant@gmail.com>
2016-10-04 08:19:59 -0400

    Add many other recipes from desktop cb

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,11 +17,35 @@ unless node['platform'] == 'debian'
   raise "Unsupported platform: #{node['platform']}"
 end
 
-include_recipe 'desktop::pc-speaker'
 include_recipe 'desktop::apt'
-include_recipe 'desktop::user'
 include_recipe 'workstation::configs'
+include_recipe 'desktop::docker'
+include_recipe 'desktop::fonts'
+include_recipe 'desktop::google-chrome'
+include_recipe 'desktop::heroku'
 include_recipe 'workstation::i3'
+include_recipe 'desktop::java'
+include_recipe 'desktop::pc-speaker'
 include_recipe 'workstation::ruby'
+include_recipe 'desktop::sbt'
+include_recipe 'desktop::scala'
+include_recipe 'desktop::slack'
+include_recipe 'desktop::spotify'
+include_recipe 'desktop::ssh'
+include_recipe 'desktop::steam'
+include_recipe 'desktop::user'
+include_recipe 'desktop::vagrant'
+include_recipe 'desktop::wine'
 
-package 'vim'
+package [
+  :gimp,
+  :inkscape,
+  :keepassx,
+  :mumble,
+  :winetricks,
+  :vim,
+  :zip
+] do
+  action :upgrade
+  timeout 3600
+end
