6c66453e7733e7aa903a7fa20c7155c417f5face
James Conant <jfaconant@gmail.com>
2016-10-13 08:18:09 -0400

    comment out recipes that require backports recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,22 +19,25 @@ end
 
 include_recipe 'desktop::apt'
 include_recipe 'workstation::configs'
-include_recipe 'desktop::docker'
 include_recipe 'desktop::fonts'
 include_recipe 'desktop::google-chrome'
 include_recipe 'desktop::heroku'
 include_recipe 'workstation::i3'
 include_recipe 'desktop::java'
 include_recipe 'desktop::pc-speaker'
-include_recipe 'workstation::ruby'
+include_recipe 'desktop::ruby'
 include_recipe 'desktop::sbt'
 include_recipe 'desktop::scala'
-# broken by upstream bug in ifupdown
-# include_recipe 'workstation::slack'
 include_recipe 'desktop::spotify'
 include_recipe 'desktop::ssh'
 include_recipe 'desktop::user'
 include_recipe 'desktop::vagrant'
+
+#
+# broken by upstream bug in ifupdown
+#
+# include_recipe 'desktop::docker'
+# include_recipe 'workstation::slack'
 
 package [
   :gimp,
