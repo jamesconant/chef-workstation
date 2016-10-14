a4611528017eccf03fe272a7a4d98948f33bce4a
James Conant <jfaconant@gmail.com>
2016-10-14 00:25:50 -0400

    contently wip

--- recipes/erebus.rb
+++ recipes/erebus.rb
@@ -7,9 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'workstation::default'
+# include_recipe 'workstation::default'
 include_recipe 'workstation::contently'
-include_recipe 'workstation::intel_wifi'
-
-package 'wicd-curses'
+# include_recipe 'workstation::intel_wifi'
+#
+# package 'wicd-curses'
 
