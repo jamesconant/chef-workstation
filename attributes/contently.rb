acbdd999382332f679b89a8046e9ddd115723dde
James Conant <jfaconant@gmail.com>
2016-10-13 22:05:48 -0400

    contently wip

--- attributes/contently.rb
+++ attributes/contently.rb
@@ -1,7 +1,5 @@
 default['postgresql']['pg_hba'] = [
+  {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'peer'},
   {:type => 'host',  :db => 'all', :user => 'all', :addr => '127.0.0.1/32',  :method => 'md5'},
-  {:type => 'host',  :db => 'all', :user => 'all', :addr => '::1/128',       :method => 'md5'},
-  {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil,             :method => 'peer'},
-  {:type => 'host',  :db => 'all', :user => 'postgres', :addr => '127.0.0.1/32',  :method => 'md5'},
-  {:type => 'host',  :db => 'all', :user => 'postgres', :addr => '::1/128',       :method => 'md5'}
+  {:type => 'host',  :db => 'all', :user => 'all', :addr => '::1/128',       :method => 'md5'}
 ]
