f577e96a85d4c54a0e710b8a8d7288846d3586e2
James Conant <jconant@tartarus>
2017-05-09 21:32:45 -0400

    more stupid

--- attributes/user.rb
+++ attributes/user.rb
@@ -2,6 +2,6 @@ default['desktop']['user'].tap do |user|
   user['name'] = 'jconant'
   user['uid'] = '1000'
   user['group'] = 'jconant'
-  user['gid'] = '1001'
+  user['gid'] = '1000'
   user['home'] = '/home/jconant'
 end
