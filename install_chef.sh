d16255ae15086cf9db8157a81480da2ffede6028
James Conant <james@conant.io>
2018-01-13 00:05:33 -0500

    Update chef install util to reference recent chefdk release

--- install_chef.sh
+++ install_chef.sh
@@ -1,7 +1,7 @@
 # http://10.0.2.2/install_chef.sh
 # Supports: debian systems only; could be easily modified for other OS package types
 
-DEB=chef_12.17.44-1_amd64.deb
+DEB=chefdk_2.4.17-1_amd64.deb
 rm -f $DEB
 wget "http://10.0.2.2:3000/$DEB"
 dpkg -i $DEB
